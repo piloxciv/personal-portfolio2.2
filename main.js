dcb34a5c1b0ba8578a6ffbc5efa6483126252cae
Roberto Levy <robertolevyadtr@gmail.com>
2022-02-07 19:13:45 -0300

    edited port sv

--- main.js
+++ main.js
@@ -79,15 +79,3 @@ const server = http.createServer(function (req, res) {
     res.end()
 })
   
-// Set up our server so it will listen on the port
-server.listen(port, function (error) {
-  
-    // Checking any error occur while listening on port
-    if (error) {
-        console.log('Something went wrong', error);
-    }
-    // Else sent message of listening
-    else {
-        console.log('Server is listening on port' + port);
-    }
-})
